e2d9cecf29fc8ac26d5b236b94fe188974d562e6
Erik-Rosenblad <erik-rosenblad@tutamail.com>
2025-01-12 18:47:32 +0000

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Site = {
+    title: string;
+    website: string;
+    description: string;
+    page_size: number;
+};
+
+export type SocialObjects = {
+    name: string;
+    href: string;
+    svgPath: string;
+}[];
