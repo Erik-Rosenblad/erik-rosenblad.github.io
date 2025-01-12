53aa69bfb85c435fb2210bd8f48d543b798897d3
Erik-Rosenblad <erik-rosenblad@tutamail.com>
2025-01-12 19:21:26 +0000

    Create config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+// Astro
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+// Constants
+const blog = defineCollection({
+    type: "content_layer",
+
+    loader: glob({
+        pattern: "**/*.md",
+        base: "./src/content/blog"
+    }),
+
+    schema: () => z.object({
+        new: z.boolean(),
+        title: z.string(),
+        minRead: z.number(),
+        pubDate: z.date(),
+        description: z.string(),
+        canonicalURL: z.string().optional(),
+    }),
+});
+
+export const collections = { blog };
