5200abdc3cf7d2d6c57ac71c5385d7ac1444d566
Erik-Rosenblad <erik-rosenblad@tutamail.com>
2025-01-12 18:48:53 +0000

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,34 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+    title: "Erik Rosenblad",
+    website: "https://erik-rosenblad.github.io",
+    description: "Personal website",
+    page_size: 6,
+};
+
+export const SOCIALS: SocialObjects = [
+    {
+        name: "Email",
+        href: "mailto:erik-rosenblad@tutamail.com",
+        svgPath: "/assets/images/tuta-logo.svg"
+    },
+
+    {
+        name: "Codeberg",
+        href: "https://codeberg.org/Erik-Rosenblad",
+        svgPath: "/assets/images/codeberg-logo.svg"
+    },
+
+    {
+        name: "GitHub",
+        href: "https://github.com/Erik-Rosenblad",
+        svgPath: "/assets/images/github-logo.svg"
+    },
+
+    {
+        name: "LinkedIn",
+        href: "https://linkedin.com/in/Erik-Rosenblad",
+        svgPath: "/assets/images/linkedin-logo.svg"
+    },
+];
